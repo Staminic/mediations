09e2c874e434114690f24cd98ab53fe93cc58c98
Simon Delmotte <simon.delmotte@staminic.com>
2019-12-12 19:01:12 +0100

    intégration du template

--- index.php
+++ index.php
@@ -3,22 +3,33 @@
 include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 
 ?>
+
 <!doctype html>
 <html lang="<?php echo $this->language; ?>">
-<head>
-    <jdoc:include type="head" />
-</head>
+  <head>
+      <jdoc:include type="head" />
+  </head>
 
-<body class="<?php echo $active->alias . ' ' . $pageclass; ?>">
+  <body class="<?php echo $active->alias . ($pageclass ? ' ' . $pageclass : ''); ?>">
+
+    <?php if ($this->countModules('content-above')) : ?>
+      <jdoc:include type="modules" name="content-above" style="html5"/>
+    <?php endif; ?>
 
     <div class="container">
+      <?php if (!empty($app->getMessageQueue)) : ?>
+        <jdoc:include type="message" />
+      <?php endif ?>
+
+      <jdoc:include type="modules" name="component-above" />
+
       <jdoc:include type="component" />
+
       <i class="fas fa-user"></i>
       <i class="far fa-user"></i>
       <i class="fab fa-github-square"></i>
     </div>
 
     <jdoc:include type="modules" name="debug" />
-</body>
-
+  </body>
 </html>
